b65d0358fc97238c8f993e02578e321a0b7ad1e6
lasso <123463530+laasso@users.noreply.github.com>
2023-12-01 13:05:58 +0100

    Update backup.sh
    1st test of the script

--- backup.sh
+++ backup.sh
@@ -1,3 +1,45 @@
-#AQUI VA EL SCRIP
+REMOTE_USER= x
+REMOTE_IP = x
+REMOTE_DIR=$REMOTE_FOLDER
+DATE=$(date +%Y-%m-%d-%H%M%S)
+BACKUP_NAME=$BACKUP_DIR/$DATE.tar.gz
+SNAPSHOT_FILE=$BACKUP_DIR/snapshot.snar
 
-#ASK THE USER FOR THE THE REMOTE CREDENTIAL AND THEN THE LOCAL FOLDERS TO IMPORT
+
+read -p "Introduce la IP del servidor remoto: " REMOTE_IP
+
+read -p "Introduce la carpeta del servidor remoto: " REMOTE_FOLDER
+
+read -p "Introduce la carpeta a respaldar (ruta completa): " SOURCE
+if [ ! -d "$SOURCE" ]; then
+	echo "Error: La carpeta no existe."
+	exit 1
+fi
+
+read -p "¿Quieres que sea un respaldo incremental? (si/no): " INCREMENTAL
+if [ "$INCREMENTAL" = "si" ]; then
+	SNAPSHOT_OPTION="-g $SNAPSHOT_FILE"
+else
+	SNAPSHOT_OPTION=""
+fi
+
+read -p "Introduce cada cuánto tiempo quieres hacer el respaldo (en minutos): " FRECUENCIA
+
+if [ ! -e "$HOME/.ssh/id_rsa" ]; then
+	echo "Configurando clave pública/privada para autenticación sin contraseña..."
+	ssh-keygen -t rsa -N "" -f "$HOME/.ssh/id_rsa"
+fi
+
+ssh-copy-id -i "$HOME/.ssh/id_rsa" $REMOTE_USER@$REMOTE_IP
+
+ssh $REMOTE_USER@$REMOTE_IP "mkdir -p $REMOTE_DIR"
+
+(crontab -l ; echo "*/$FRECUENCIA * * * * $0") | crontab -
+
+backup() {
+	tar -cvzpf $BACKUP_NAME $SNAPSHOT_OPTION $SOURCE
+	scp $BACKUP_NAME $REMOTE_USER@$REMOTE_IP:$REMOTE_DIR
+	scp $SNAPSHOT_FILE $REMOTE_USER@$REMOTE_IP:$REMOTE_DIR
+}
+
+backup
